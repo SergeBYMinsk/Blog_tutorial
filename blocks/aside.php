91e5b4c13dbf699a039065624fa7b1325bc483c9
serge_by_minsk <sergebyminsk@icloud.com>
2019-11-08 13:54:46 +0300

    add all file in this project

---
+++ blocks/aside.php
@@ -0,0 +1,9 @@
+<aside class="col-md-4">
+  <div class="p-3 mb-3 bg-warning rounded">
+    <h4><b>Добро пожаловать!</b></h4>
+    <p>БЕЛОРУССКИЙ ГОСУДАРСТВЕННЫЙ УНИВЕРСИТЕТ ИНФОРМАТИКИ И РАДИОЭЛЕКТРОНИКИ</p>
+  </div>
+  <div class="p-3 mb-3">
+    <img class="img-thumbnail" src="/img/favicon2.ico">
+  </div>
+</aside>
---
+++ blocks/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+  <div class="container">
+    <span class="text-muted">Все права защищены</span>
+</footer>
